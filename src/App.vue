f9648dc46edebb3366e4d1b07870838dbcd1dfb3
이영현 <naem77716@gmail.com>
2026-01-13 14:34:50 +0900

    프로젝트 초기 셋팅 중

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,12 @@
 <template>
   <div class="wrap">
-    <HeaderBanner/>
-    <Header></Header>
-    <HeaderMenu />
-    <HomePage/>
-    <SnsMainComponent/>
-    <Footer></Footer>
-</div>
+    <Home />
+  </div>
 </template>
 
-<script>
-import HeaderBanner from './components/HeaderBanner.vue';
-import Header  from './components/Header.vue'
-import HeaderMenu from './components/HeaderMenu.vue';
-import HomePage from './components/HomePage.vue'
-import SnsMainComponent from './components/SnsMainComponent.vue'
-import Footer from './components/Footer.vue';
-
-
-export default {
-  name: "App",
-  components: { 
-    HeaderBanner,
-    Header,
-    HeaderMenu,
-    HomePage,
-    SnsMainComponent,
-    Footer,
-  },
-};
+<script setup>
+import Home from './components/Home.vue';
 </script>
 
 <style>
-  .commLayout{
-    display: block;
-    max-width: 1200px;
-    width: 100%;
-    margin: 0 auto;
-  }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    테스트 문장입니다 확인해 주세요
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+  // 지역 스타일로 해당 컴퍼넌트에만 스타일 적용됩니다.
+
+</style>
